4b8a9c81f73e475efbbe8dfaf12445c5f044da89
Andres527 <85116170+Andres527@users.noreply.github.com>
2025-10-16 14:27:40 -0500

    Add use_unicode option to database configuration

--- app/config/db_config.py
+++ app/config/db_config.py
@@ -9,6 +9,7 @@ def get_db_connection():
         database=os.getenv("MYSQL_DATABASE"),
         port=int(os.getenv("MYSQL_PORT", 3306)),
         charset='utf8mb4', 
-        collation='utf8mb4_unicode_ci'
+        collation='utf8mb4_unicode_ci',
+        use_unicode=True
     )
 
