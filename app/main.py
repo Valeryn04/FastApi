383e3cfa4355f48bd7701da922d604b77e9718ad
Valeryn006 <thvdumplvv@gmail.com>
2025-10-16 12:39:45 -0500

    0

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ origins = [
     "http://localhost:8000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://proyecto3-azure.vercel.app/",
+    "https://proyecto3-azure.vercel.app",
     
 ]
 
